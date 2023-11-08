12b9655cf6c4bb635244ee86483d795ca678c4e1
GabrielAchumba <achumba.gabriel@yahoo.com>
2023-11-08 07:17:46 +0100

    Added Login Dialog to Second Page

--- src/components/shared/S-TitleBar.vue
+++ src/components/shared/S-TitleBar.vue
@@ -3,11 +3,41 @@
          <q-bar 
             class="col-12 bg-positive text-accent"
             style="height:10vh;">
-            <div>
+            <q-list>
+                <q-item v-ripple>
+                    <q-item-section avatar>
+                        <q-avatar>
+                            <img :src="src">
+                        </q-avatar>
+                    </q-item-section>
+
+                    <q-item-section>
+                        <q-item-label class="text-accent">GLOBAL IT</q-item-label>
+                        <q-item-label caption lines="1" class="text-accent">communications</q-item-label>
+                    </q-item-section>
+                </q-item>
+            </q-list>
+
+            <!-- <q-btn class="text-accent bg-positive" no-caps>
+                <div class="row">
+                    <q-avatar>
+                        <img :src="src">
+                    </q-avatar> 
+                    <p>
+                        <span>
+                            <p>GLOBAL IT</p>
+                            <p class="text-caption">COMMUNICATIONS</p>
+                        </span>
+                    </p>
+                </div>   
+            </q-btn> -->
+
+            <!-- <div>
                 <q-avatar>
                     <img :src="src" class="text-accent">
                 </q-avatar>
-            </div>
+            </div> -->
+
             <q-space/>
             <q-btn 
             no-caps
